2c948be99b90042edf7bc0a6367f1b9c2587815f
codywakeford <codypwakeford@gmail.com>
2025-03-11 07:54:47 +0000

    fixed dir

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,16 @@
-/home/cody/git/nuxt-website-template/jest.config.js
+export default {
+    preset: "ts-jest", // for TypeScript projects
+    testEnvironment: "node", // or 'jsdom' for browser-like environments
+    transform: {
+        "^.+\\.[tj]sx?$": "babel-jest", // Use babel-jest to handle TypeScript/JS
+    },
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+    testMatch: ["**/unit/?(*.)+(spec|test).[tj]s?(x)"], // Test file naming pattern
+    coverageDirectory: "coverage", // output directory for coverage reports
+    collectCoverageFrom: ["src/**/*.{js,ts,jsx,tsx}"], // coverage includes files in src
+    globals: {
+        "ts-jest": {
+            useBabelrc: true, // Use babel config if present
+        },
+    },
+}
